d28c1c32bcd68d1a548b505be39f5b2e67db2559
XOPOIIINY <107848820+XOPOIIINY@users.noreply.github.com>
2024-02-14 17:30:22 +0300

    Task 2 - complete!

---
+++ task_2.py
@@ -0,0 +1,20 @@
+from datetime import datetime, timedelta
+import csv
+
+with open('songs.csv', 'r', encoding='utf-8') as csv_f:
+    data = list(csv.reader(csv_f, delimiter=';'))
+
+
+def bubble_sort(data):
+    for i in range(len(data) - 1):
+        for j in range(len(data) - 1):
+            if datetime.strptime(data[j][-1], '%d.%m.%Y') < datetime.strptime(data[j + 1][-1], '%d.%m.%Y'):
+                data[j], data[j + 1] = data[j + 1], data[j]
+    return data[::-1]
+
+sorted_data = bubble_sort(data[1:])
+print(f'{1} {sorted_data[0][2]}, {sorted_data[0][1]}, {sorted_data[0][0]}')
+print(f'{2} {sorted_data[1][2]}, {sorted_data[1][1]}, {sorted_data[1][0]}')
+print(f'{3} {sorted_data[2][2]}, {sorted_data[2][1]}, {sorted_data[2][0]}')
+print(f'{4} {sorted_data[3][2]}, {sorted_data[3][1]}, {sorted_data[3][0]}')
+print(f'{5} {sorted_data[4][2]}, {sorted_data[4][1]}, {sorted_data[4][0]}')
